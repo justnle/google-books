515e47e4f93c638ce3f1916f1be4775a3d89310b
Justin <twoplatinumchainz@gmail.com>
2020-04-26 23:22:01 -0700

    Deleted whitespace

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,7 +5,7 @@ export default {
         return axios.get(`/api/books`);
     },
     deleteBook(id) {
-        return axios.delete(`/api/books/${ id}`);
+        return axios.delete(`/api/books/${id}`);
     },
     saveBook(bookData) {
         return axios.post(`/api/books`, bookData);
