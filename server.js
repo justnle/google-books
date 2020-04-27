20dd31fcb729f38f41bb243cda5b31884fe6c94f
Justin <twoplatinumchainz@gmail.com>
2020-04-26 22:47:41 -0700

    Updated loclahost url

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === `production`) {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/reactreadinglist`, {
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/googlebooksearch`, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useCreateIndex: true
