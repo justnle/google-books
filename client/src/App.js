b45b160eb8791cbeaffba081b9bccf653cac1e8a
Justin <twoplatinumchainz@gmail.com>
2020-04-26 21:52:04 -0700

    Redirected route paths to new components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Books from './pages/Books';
-import Detail from './pages/Detail';
+import Saved from './pages/Saved';
 import Nav from './components/Nav';
 import NoMatch from './pages/NoMatch';
 import React from 'react';
@@ -12,8 +12,8 @@ function App() {
                 <Nav />
                 <Switch>
                     <Route exact path="/" component={Books} />
-                    <Route exact path="/books" component={Books} />
-                    <Route exact path="/books/:id" component={Detail} />
+                    <Route exact path="/search" component={Books} />
+                    <Route exact path="/saved" component={Saved} />
                     <Route component={NoMatch} />
                 </Switch>
             </div>
