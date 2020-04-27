aeab9e810cf61694b1fa71ced79b282990897f62
Justin <twoplatinumchainz@gmail.com>
2020-04-27 14:39:20 -0700

    Extracted SearchBar and Results into components

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -1,5 +1,6 @@
-import { Col, Container, Row } from '../components/Grid';
-import { FormBtn, Input } from '../components/Form';
+import { Container } from '../components/Grid';
+import SearchBar from '../components/SearchBar';
+import Results from '../components/Results';
 import React, { useState } from 'react';
 import API from '../utils/API';
 import Jumbotron from '../components/Jumbotron';
@@ -8,104 +9,31 @@ function Books() {
   const [books, setBooks] = useState([]);
   const [formObject, setFormObject] = useState({});
 
-  function handleInputChange(event) {
+  const handleInputChange = (event) => {
     const { name, value } = event.target;
     setFormObject({ ...formObject, [name]: value });
-  }
+  };
 
-  function handleFormSubmit(event) {
+  const handleFormSubmit = (event) => {
     event.preventDefault();
     API.searchBook(formObject.title)
       .then((res) => setBooks(res.data.items))
       .catch((err) => console.error(err));
-  }
-
-  function saveBook(bookData) {
-      API.saveBook(bookData)
-      .then(() => console.log(`saved book`))
-      .catch(err => console.error(err));
-  }
+  };
 
   return (
     <Container fluid>
       <Jumbotron>
         <h1>Google Books Search</h1>
       </Jumbotron>
-      <Row>
-        <Col size='md-12'>
-          <h3>Search</h3>
-          <Col size='md-12'>
-            <form>
-              <Input
-                onChange={handleInputChange}
-                name='title'
-                placeholder='Search for a book title!'
-              />
-              <FormBtn disabled={!formObject.title} onClick={handleFormSubmit}>
-                Search
-              </FormBtn>
-            </form>
-          </Col>
-        </Col>
-      </Row>
-
-      <Row>
-        <Col size='md-12'>
-          <h3>Results</h3>
-        </Col>
-        <div className='col-md-12'>
-          {books.length ? (
-            books.map((book) => (
-              <div className='row py-2' key={book.id}>
-                <div className='col-md-3 my-auto'>
-                  <img
-                    className='img-fluid'
-                    src={book.volumeInfo.imageLinks.thumbnail}
-                    alt='book-cover'
-                    style={{ width: '100%' }}
-                  ></img>
-                </div>
-                <Col size='md-9'>
-                  <div className='card-body'>
-                    <h6 className='card-title'>{book.volumeInfo.title}</h6>
-                    <h6 className='card-subtitle mb-2 text-muted'>
-                      {book.volumeInfo.authors}
-                    </h6>
-                    <p className='card-text'>{book.volumeInfo.description}</p>
-                    <a
-                      href={book.volumeInfo.infoLink}
-                      className='btn btn-primary'
-                      target='_blank'
-                      rel='noopener noreferrer'
-                    >
-                      View
-                    </a>
-                    <button
-                      type='button'
-                      className='btn btn-info'
-                      onClick={() => 
-                      saveBook({
-                          title: book.volumeInfo.title,
-                          id: book.id,
-                          author: book.volumeInfo.authors[0],
-                          description: book.volumeInfo.description,
-                          image: book.volumeInfo.imageLinks.thumbnail,
-                          link: book.volumeInfo.infoLink
-                      })}
-                    >
-                      Save
-                    </button>
-                  </div>
-                </Col>
-              </div>
-            ))
-          ) : (
-            <div className='text-center'>
-              <h3>No Results to Display</h3>
-            </div>
-          )}
-        </div>
-      </Row>
+      <Container>
+        <SearchBar
+          input={handleInputChange}
+          disabled={formObject.title}
+          submit={handleFormSubmit}
+        />
+        <Results data={books} />
+      </Container>
     </Container>
   );
 }
