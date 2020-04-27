6699ffe92ef0f8c174bd0067c7bd6b3b9a06b7c7
Justin <twoplatinumchainz@gmail.com>
2020-04-26 19:42:57 -0700

    Added nav items for pages

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,9 +3,17 @@ import React from 'react';
 function Nav() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a className="navbar-brand" href="/">
-                React Reading List
+            <a className="navbar-brand">
+                Google Books
             </a>
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a className="nav-link" href="/">search</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/saved">saved</a>
+                </li>
+            </ul>
         </nav>
     );
 }
