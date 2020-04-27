f9b3dbe54a131bbab85a056fe8dd937d4adf0530
Justin <twoplatinumchainz@gmail.com>
2020-04-26 21:58:53 -0700

    Readded date

--- models/book.js
+++ models/book.js
@@ -8,7 +8,8 @@ const bookSchema = new Schema({
   author: { type: String, required: true },
   description: String,
   image: { type: String, required: true },
-  link: { type: String, required: true }
+  link: { type: String, required: true },
+  date: { type: Date, default: Date.now }
 });
 
 const Book = mongoose.model(`Book`, bookSchema);
